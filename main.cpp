bad1580eafd318b18c0fe7ba48a38c95572ac03f
EVGENIY SILCHENKO <eve.ashumi@gmail.com>
2016-11-27 05:13:22 +0300

    Добавлены объекты QLineEdit. Копирование данных из одного QLineEdit в другой.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,21 @@ int main(int argc, char *argv[])
     QApplication app(argc,argv);
 
     QWidget *widget = new QWidget;
+    QPushButton *quitButton = new QPushButton("Quit");
+    QLineEdit *first = new QLineEdit();
+    QLineEdit *second = new QLineEdit();
+    QVBoxLayout *buttonLayout = new QVBoxLayout;
 
+    buttonLayout->addWidget(first);
+    buttonLayout->addWidget(second);
+    buttonLayout->addWidget(quitButton);
+
+    QObject::connect(quitButton,SIGNAL(clicked()),qApp,SLOT(quit()));
+    QObject::connect(first,SIGNAL(textEdited(QString)),second,SLOT(setText(QString)));
+    QObject::connect(second,SIGNAL(textEdited(QString)),first,SLOT(setText(QString)));
+
+    widget->setLayout(buttonLayout);
+    widget->setWindowFlags(Qt::Window);
     widget->show();
 
     return app.exec();
